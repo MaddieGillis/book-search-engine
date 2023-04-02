34380bc60999b4fbef6961cda159f097e3d7f395
MaddieGillis <112782694+MaddieGillis@users.noreply.github.com>
2023-04-01 19:43:25 -0700

    Completed resolvers:

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,14 +3,65 @@ const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id}).select('-__v -password');
-                return userData;
-            }
-            throw new AuthenticationError('Please log in to view page');
-        }
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id}).select('-__v -password');
+        return userData;
+      }
+      throw new AuthenticationError('Please log in to view page');
     }
-},
+  },
+  
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+  
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+        throw new AuthenticationError('User not found');
+      }
+  
+      const correctPw = await user.isCorrectPassword(password);
+  
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password')
+      }
+  
+      const token = signToken(user);
+      return { token, user };
+  
+    },
+  
+    saveBook: async (parent, { newBook }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: newBook }},
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('Please log in to use this feature');
+    },
+  
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId }}},
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('Please log in to use this feature')
+    }
+  }
+};
 
+module.exports = resolvers;
