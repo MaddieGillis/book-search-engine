bd4db38f96f6508e3c25a18bff493e8e614eb058
MaddieGillis <112782694+MaddieGillis@users.noreply.github.com>
2023-04-02 18:57:11 -0700

    reconfigured apollo-client to connect to db

--- client/src/apollo-client.js
+++ client/src/apollo-client.js
@@ -1,9 +1,12 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:3001/graphql',
-    cache: new InMemoryCache()
-  });
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 export default client;
